2c839d41d1a51ec80937363e0709110a15cece36
kaoutar25 <kaoutar.benabdesselam20@gmail.com>
2023-02-16 14:45:35 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"programming is like buildung a multilingual puzzle");
+        return (0);
+}
