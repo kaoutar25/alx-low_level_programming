137e87e96671dd063c7ea54e840f44e9f189fd9d
kaoutar25 <kaoutar.benabdesselam20@gmail.com>
2023-02-21 17:08:02 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,9 +20,19 @@ void times_table(void)
 				d = (z - u) / 10;
 				_putchar(44);
 				_putchar(32);
-				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
-			_putchar(z + '0');
+			else
+			{
+				if (y !=0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
 		}
 	}
 	_putchar('\n');
